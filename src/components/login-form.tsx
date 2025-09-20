f6be450fa80cc2353ffc09a767b1687f8517b71d
MENGLAY <tongmenglaysmae5@gmail.com>
2025-09-20 13:27:17 +0700

    Remove SignupForm and SignupPage components to streamline registration process. Update LoginForm to eliminate signup link, enhancing user flow.

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -52,12 +52,6 @@ export function LoginForm({
                 Login
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="/register" className="underline underline-offset-4">
-                Sign up
-              </Link>
-            </div>
           </form>
         </CardContent>
       </Card>
